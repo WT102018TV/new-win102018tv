98d348edd97cde3b192950ab4462a4286199cc2e
Aleksei Linuxoidov <aleksejlapockin1@gmail.com>
2021-10-17 19:57:54 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,237 @@
+<!doctype html>
+<html lang="ru">
+<head>
+<title>W102018TV - Лучший среди конкуретнов </title>
+<meta charset="utf-8">
+<meta name="description" content="МифСофт - это чистые сборки Windows с приятным интерфейсом.">
+<meta name="keywords" content="mifsoft,сборки,Windows">
+<meta name="author" content="Feralcas">
+<meta name="copyright" content="МифСофт">
+<meta name="google-site-verification" content="cO1o6sx54cvKxhbnYsABWtl4sYFj9uVKV0DxLKZkWv8"/>
+<?php include 'lib/module/sys-meta.php';?>
+<?php include 'lib/module/sys-2.php';?>
+</head>
+<?php include 'lib/module/sys-php.php';?>
+  <!--[if lt IE 9]><script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+  <meta name="yandex-verification" content="6d503e7caac40356" />
+  <meta name="yandex-verification" content="6d503e7caac40356" />
+  </head> 
+  <body>
+<div class="wrap">
+	<div class="main center">
+
+		<data>
+			<a>
+			<div class="search-box">
+                <formo>
+					<inputsasas>
+					<inputsa>
+					<div>
+						<inputasas>
+						
+					</div>
+				</formg>									
+
+			</div>       
+		</header>
+		<div class="cols clearfix" id="cols">
+			<div class="content">
+				
+				<div class="main-title clearfix">
+			    </div>
+				<div class="shotdiv">
+				<div class="floaters clearfix grid grid-list" data-view="grid-list " id="grid">
+
+
+
+				
+				
+                
+				
+				
+				
+				
+				
+				    
+					<div id='dle-content'><div class="mov clearfix">
+	<div class="mov-i img-box">
+		<img src="/img/img-sborki/w8-sborki.png" alt= />
+		<div class="mov-mask flex-col ps-link" data-link="/sborki/sborka-windows-8-by-w102018tv.php"><span class="fa fa-play"></span></div>
+		
+
+	</div>
+	<a class="mov-t nowrap" href="/sborki/sborka-windows-8-by-w102018tv.php">Windows 8 BY WIN2018TV</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">Windows 8 </div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Microsoft</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc"> All </div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc"> 64 - 32</div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc"> Нет </div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 4.72 гб (64 bit), 3.65 гб (32 bit)  </div></li>
+			</ul>
+			<div class="movie-desc2">Сборка только с нужными програмами ничего не вырезано,интегрированы все последние 
+				обновления!</div>
+			</div>
+</div><div class="mov clearfix">
+	<div class="mov-i img-box">			
+ 	<img src="/img/img-sborki/w1021h1-sborki.png" alt= />
+		<div class="mov-mask flex-col ps-link" data-link="/sborki/windows-10-21h1.php"><span class="fa fa-play"></span></div>
+
+
+	</div>
+	<a class="mov-t nowrap" href="/sborki/windows-10-21h1.php">Windows 10 21HI BY WIN2018TV 2.0</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">21HI</div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Microsoft</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc"> All </div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc"> 64 - 32</div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc"> Осутвет </div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 6,14 гб </div></li>
+			</ul>
+			<div class="movie-desc2">Сборка только с нужными програмами ничего не вырезано,интегрированы все последние 
+				обновления!</div>
+			</div>
+</div><div class="mov clearfix">
+	<div class="mov-i img-box">
+		<img src="/img/img-sborki/w7-sborki.png" alt= />
+		<div class="mov-mask flex-col ps-link" data-link="/sborki/sborka-windows-7-by-w102018tv.php"><span class="fa fa-play"></span></div>
+		
+			
+			
+	</div>
+	<a class="mov-t nowrap" href="/sborki/sborka-windows-7-by-w102018tv.php">Windows 7 BY WIN2018TV 2.0</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">Windows 7 SP1</div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Microsoft</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc"> All </div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc"> 64 - 32</div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc"> Осутвет </div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 5.81 гб (64 bit), 4.57 гб (32 bit)  </div></li>
+			</ul>
+			<div class="movie-desc2">Сборка только с нужными програмами ничего не вырезано,интегрированы все последние 
+				обновления!</div>
+			</div>
+</div><div class="mov clearfix">
+	<div class="mov-i img-box">
+		<img src="/img/img-sborki/w10-sborki.png" alt= />
+		<div class="mov-mask flex-col ps-link" data-link="/sborka-windows-10-ltsc-by-w102018tv.php"><span class="fa fa-play"></span></div>
+		
+			
+			
+	</div>
+	<a class="mov-t nowrap" href="/sborki/sborka-windows-10-ltsc-by-w102018tv.php">Windows 10 ltsc by Win102018tv</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">Последняя</div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Сообщество</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc"> All </div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc"> all </div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc"> Ненужна </div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 18 мб </div></li>
+			</ul>
+			<div class="movie-desc2">Сборка только с нужными програмами ничего не вырезано,интегрированы все последние 
+				обновления!</div>
+			</div>
+</div><div class="mov clearfix">
+	<div class="mov-i img-box">
+	<img src="/img/img-sborki/j2dp02z1w1.png" alt= />
+		<div class="mov-mask flex-col ps-link" data-link="/programs/mpv.php"><span class="fa fa-play"></span></div>
+		
+			
+			
+	</div>
+	<a class="mov-t nowrap" href="/programs/mpv.php">MPV - Самый быстрый видеоплеер</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">Последняя</div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Сообщество</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc"> All </div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc"> all </div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc"> Ненужна </div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 18 мб </div></li>
+			</ul>
+			<div class="movie-desc2">Этот видеоплеер по праву можно назвать самым шустрым!
+Он не только поддерживает практически все существующие форматы медиафайлов, но и открывает их молниеносно!</div>
+			</div>
+</div><div class="mov clearfix">
+	<div class="mov-i img-box">
+	<img src="/img/img-sborki/picasa.jpg" alt="Picasa" />
+		<div class="mov-mask flex-col ps-link" data-link="/programs/picasa.php"><span class="fa fa-play"></span></div>
+		
+			
+			
+	</div>
+	<a class="mov-t nowrap" href="/programs/picasa.php">Google Picasa 3</a>
+	<div class="movie-text">
+			<ul class="movie-lines">
+				<li><div class="ml-label">Версия:</div><div class="ml-desc">Google Picasa 3.9</div></li>
+				<li><div class="ml-label">Разработчик: </div> <div class="ml-desc">Google</div></li>
+				<li><div class="ml-label">Язык интерфейса:</div> <div class="ml-desc">Мультиязычный</div></li>
+				<li><div class="ml-label">Разрядность:</div> <div class="ml-desc">Обе</div></li>
+				<li><div class="ml-label">Лечение:</div> <div class="ml-desc">Не Требует</div></li>
+			    <li><div class="ml-label">Размер:</div> <div class="ml-desc"> 13 мб </div></li>
+			</ul>
+			<div class="movie-desc2">Вам надоел стандартный просмотрщик картинок? Он слишком уродливый, медленный или Вы просто хотите чего-то получше?
+Тогда <span style="color: #8ce471;"><strong>Google Picasa</strong></span> это Ваше спасение!!!</div>
+
+		
+			
+			
+	
+		
+
+
+
+				
+
+
+
+
+
+
+
+
+
+
+
+			
+			
+
+	</div>
+</div><div class="clr"></div>
+<div class="pagi-nav clearfix ignore-select">
+	
+	<span class="navigation"><span>1</span>		
+	<a href="/pages/2.php">2</a>																																																																																																																														
+</div></div>
+				</div></div>
+				
+					
+			
+				
+			</div>		
+				
+				
+                
+				
+				
+				
+				
+	
+
+					
+			<!-- end content -->
+<?php include 'lib/module/sys-sidebar.php';?>
+					
+
+
+                
+			</aside>
+
+
+
+
+</html>
